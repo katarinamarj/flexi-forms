cc06e874e427e97e8354956f1e2131b036c82232
katarinamarj <katarinamarjanovic02@gmail.com>
2025-03-12 22:57:35 +0100

    Create login page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,16 @@
-import { useEffect, useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("https://127.0.0.1:8000/api/message")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
   return (
-    <div>
-      <h1>React + Symfony</h1>
-      <p>Message from backend: {message}</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,8 @@
+import "../styles/Register.css";
+
+const RegisterPage = () => {
+    return <div>Register Page</div>;
+  };
+  
+  export default RegisterPage; 
+  

5b31a60d09517502e20338e6984c4d15f62ddaa0
katarinamarj <katarinamarjanovic02@gmail.com>
2025-03-13 00:40:23 +0100

    Implemented login-functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="*" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,6 @@
+import "../styles/Dashboard.css";
+const Dashboard = () => {
+    return <div>Dashboard</div>;
+  };
+  
+export default Dashboard; 

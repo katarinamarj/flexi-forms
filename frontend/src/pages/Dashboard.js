f30705446885c6e45d1c4d350f3408eb7f85cc95
katarinamarj <katarinamarjanovic02@gmail.com>
2025-04-03 21:08:28 +0200

    Dashboard linkend to the routes in FormTemplate controller

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,11 +1,70 @@
+import { useEffect, useState } from "react";
 import "../styles/Dashboard.css";
 import Header from "../components/Header"; 
+import Footer from "../components/Footer"; 
+
 
 const Dashboard = () => {
-    return <div>
-      <Header />
-      Dashboard
-      </div>;
+  
+  const [templates, setTemplates] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    fetchTemplates();
+  }, []);
+
+  const fetchTemplates = async () => {
+    const response = await fetch("https://localhost:8000/form-templates");
+    const data = await response.json();
+    setTemplates(data);
   };
+
+  const createTemplate = async () => {
+    await fetch("https://localhost:8000/form-templates", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, description }),
+    });
+    setName("");
+    setDescription("");
+    fetchTemplates();
+  };
+
+
+  return (
+  <div>
+    <Header />
+    
+    <div className="dashboard-container">
+      <h2>Create Form Template</h2>
+      <div className="form-container">
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <textarea
+          type="text"
+          placeholder="Description"
+          value={description}
+          rows={4}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={createTemplate}>Create</button>
+      </div>
+    </div>
+
+
+
+    <Footer />
+
+  </div>
+  )
+};
   
 export default Dashboard; 
